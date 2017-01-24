b37f6ee60df43237a49e3b2cee006d8f91ab5d74
DanielOlvera <josue_olver@yahoo.com.mx>
2017-01-24 11:44:18 -0500

    Added buttons for the examples about the different options of storage in android

--- app/src/main/java/com/example/daniel/storage_example/MainActivity.java
+++ app/src/main/java/com/example/daniel/storage_example/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.daniel.storage_example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void wSharedPrefs(View view) {
+    }
+
+    public void wInternalStrg(View view) {
+    }
+
+    public void wExternalStrg(View view) {
+    }
+
+    public void wSQLiteDB(View view) {
+    }
+
+    public void wNetworkConnection(View view) {
     }
 }
