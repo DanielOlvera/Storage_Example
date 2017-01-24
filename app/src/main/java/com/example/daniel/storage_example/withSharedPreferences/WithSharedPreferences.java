91d454ad6caf15bde7d51a96c64a9fd2c53928e0
DanielOlvera <josue_olver@yahoo.com.mx>
2017-01-24 12:03:13 -0500

    SharedPreferencesSample: Added package and empty activity, added label and parentActivity in the Manifest

---
+++ app/src/main/java/com/example/daniel/storage_example/withSharedPreferences/WithSharedPreferences.java
@@ -0,0 +1,13 @@
+package com.example.daniel.storage_example.withSharedPreferences;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WithSharedPreferences extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_with_shared_preferences);
+    }
+}
